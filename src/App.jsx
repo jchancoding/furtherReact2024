5ff42ebd4294f995f372a85dc59dec15de18760e
Jasper Chan <32831824+jchancoding@users.noreply.github.com>
2024-10-10 11:34:55 -0700

    Initialize App
    Initialized vite react app with comparison of counters using State vs Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import { useState, useReducer } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
+  // create a reducer, initial value is 0
+  const [state, dispatch] = useReducer(reducer, 0);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      default:
+        return state;
+    }
+  }
 
   return (
     <div>
-      {count}
+      Use State: {count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      Use Reducer: {state}
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
     </div>
   );
 }
